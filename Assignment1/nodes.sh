781fafea13c432b79fb875a6e2c843e3eb5b65d4
Bert Peters <bert.ljpeters@gmail.com>
2015-09-16 10:24:37 +0200

    Vervang sort compressie programma door gzip.
    XZip (LZMA) was wat intensief voor mijn arme laptop, en gzip is bijna
    net zo goed en minder duur om te berekenen.

--- Assignment1/nodes.sh
+++ Assignment1/nodes.sh
@@ -10,5 +10,5 @@ for file in "$@"
 do
 	echo -n "$file: "
 
-	awk '{print $1;print $2}' "$file" | sort -u --compress-program xz -S 30% | wc -l
+	awk '{print $1;print $2}' "$file" | sort -u --compress-program gzip -S 10% | wc -l
 done
