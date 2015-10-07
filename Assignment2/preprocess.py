9794340e7ef21b986a788c6cd9c3af28dad893ba
Bert Peters <bert.ljpeters@gmail.com>
2015-10-07 10:08:22 +0200

    Voeg een preprocessor toe.

---
+++ Assignment2/preprocess.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2.7
+
+from __future__ import print_function
+import fileinput
+import re
+import sys
+
+def userCode(name):
+    if name not in userCode.mapping:
+        userCode.maxCode = userCode.maxCode + 1
+        userCode.mapping[name] = userCode.maxCode
+
+    return userCode.mapping[name]
+
+userCode.maxCode = 0
+userCode.mapping = {}
+
+
+def main():
+    usernamePattern = re.compile('@([a-zA-Z0-9_]+)')
+
+    for line in fileinput.input():
+        parts = line.split("\t", 2)
+        timestamp = parts[0]
+        user = userCode(parts[1].lower())
+
+        for mention in usernamePattern.findall(parts[2]):
+            print(str(user) + "\t" + str(userCode(mention.lower())) +  "\t" + timestamp)
+
+    for username,code in userCode.mapping.items():
+        print(code, username, file=sys.stderr)
+
+if __name__ == "__main__":
+    main()
